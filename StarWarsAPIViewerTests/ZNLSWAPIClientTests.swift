cb7d40c395e8c876c3065b5fb15c383d7a712adb
Emil Vaklinov <emil_vaklinov@yahoo.com>
2023-06-30 14:22:00 +0100

    Adding API client unit test

--- StarWarsAPIViewerTests/ZNLSWAPIClientTests.swift
+++ StarWarsAPIViewerTests/ZNLSWAPIClientTests.swift
@@ -10,5 +10,49 @@ import XCTest
 
 class ZNLSWAPIClientTests: XCTestCase {
 
-    //TODO: Unit test the SWAPIClient
+    func testBeginRequestForEndpoint() {
+        let baseURL = URL(string: "https://swapi.dev/api/")!
+        let session = MockURLSession(configuration: URLSessionConfiguration.ephemeral)
+        let apiClient = ZNLSWAPIClient(baseURL: baseURL, using: session)
+        
+        let endpoint = "planets"
+        let expectation = XCTestExpectation(description: "Network request completion")
+        
+        apiClient.beginRequest(forEndpoint: endpoint) { (data, error) in
+            // Assert the completion behavior
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        
+        // Verification that the correct URL was used
+        XCTAssertEqual(session.lastRequest?.url?.absoluteString, "https://swapi.dev/api/planets")
+        
+        // Simulate a successful response
+        let response = HTTPURLResponse(url: baseURL, statusCode: 200, httpVersion: nil, headerFields: nil)
+        let data = Data()
+        session.lastCompletionHandler?(data, response, nil)
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    // Create a mock URLSession for testing
+    class MockURLSession: URLSession {
+        var lastRequest: URLRequest?
+        var lastCompletionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+        
+        init(configuration: URLSessionConfiguration) {
+            super.init()
+        }
+      
+        class func sessionWithConfiguration(configuration: URLSessionConfiguration) -> URLSession {
+            return URLSession(configuration: configuration)
+        }
+
+        override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            lastRequest = request
+            lastCompletionHandler = completionHandler
+            return URLSession.shared.dataTask(with: request, completionHandler: completionHandler)
+        }
+    }
 }
